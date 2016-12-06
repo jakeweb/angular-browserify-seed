3b1856786c33165143347ac7f8aacc08cbd386e9
Yevhenii Tanasiichuk <yetanua@hotmail.com>
2016-12-06 16:26:05 +0200

    add infinite-scroll

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,10 +3,11 @@
 // browserify-shim dependencies
 require('angular');
 require('angular-route');
-require('angular-animate');
+// require('angular-animate');
 require('angular-aria');
-require('angular-material');
+// require('angular-material');
 require('angular-translate');
+require('ng-infinite-scroll');
 
 // application initialization
 require('./app');
